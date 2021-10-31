50f30d63954b2faef80548acddb0cc1f645bb5a5
kaustubh <kaustubhkj@gmail.com>
2021-10-31 18:23:52 +0530

    crawler spoofing mechanism

--- src/background/bypassLogic.js
+++ src/background/bypassLogic.js
@@ -7,6 +7,8 @@ function setTwitterHeader(details) {
         "name": "Referer",
         "value": "https://t.co/"
     })
+
+    return {requestHeaders: details.requestHeaders};
 }
 
 chrome.webRequest.onBeforeSendHeaders.addListener(setTwitterHeader, {
@@ -14,3 +16,30 @@ chrome.webRequest.onBeforeSendHeaders.addListener(setTwitterHeader, {
     types: ["main_frame"], },
     ["requestHeaders", "blocking", "extraHeaders"]
 );
+
+function crawlerSpoofer(details) {
+    var google_adbot_UA = "AdsBot-Google (+http://www.google.com/adsbot.html)"
+    details.requestHeaders = details.requestHeaders.filter(function(header) {
+        if(header.name === "User-Agent" || header.name === "X-Forwarded-  For") return false;
+        return true
+    })
+
+    details.requestHeaders.push({
+        "name": "User-Agent",
+        "value": google_adbot_UA
+    })
+    details.requestHeaders.push({
+        "name": "X-Forwarded-For",
+        "value": "66.102.0.0"
+    })
+    
+    return {requestHeaders: details.requestHeaders};    
+}
+
+// chrome.webRequest.onBeforeSendHeaders.addListener(crawlerSpoofer, {
+//     urls: ["<all_urls>"],
+//     types: ["main_frame"], },
+//     ["requestHeaders", "blocking", "extraHeaders"]
+// );
+
+// todo -> make crawlerSpoofer conditional and add cookie blocking too
