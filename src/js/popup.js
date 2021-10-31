135f54e01b4cea1a95d506d9777d38bb360b2ac7
kaustubh <kaustubhkj@gmail.com>
2021-10-31 12:54:33 +0530

    initialization

---
+++ src/js/popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('checkPage');
+
+    checkPageButton.addEventListener('click', function() {
+        
+        chrome.tabs.getSelected(null, function(tab) { 
+            alert(tab.url)
+        });
+        
+    });
+});
